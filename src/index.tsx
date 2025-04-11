d96d046593c34c80fafce875909005eff935b42c
HocusLocusT <182385655+HocusLocusTee@users.noreply.github.com>
2025-04-11 19:56:47 +1000

    feat: improved navigation
    - change router to router-dom
    - added key to lazy load

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-// import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import init, { initConsolePanicHook } from "kaspa-wasm";
 import { ModuleLayout } from "./modules/ModuleLayout";
@@ -22,15 +21,15 @@ const setup = async () => {
   root.render(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<App />} />
         <Route path="/modules" element={<ModuleLayout />}>
           {modules.map((m) => (
             <Route
-              key={m.path}
-              path={m.path}
-              element={<Lazy component={m.component}></Lazy>}
-            ></Route>
-          ))}
+            key={m.path}
+            path={m.path}
+            element={<Lazy key={m.path} component={m.component} />}
+          />
+                ))}
         </Route>
       </Routes>
     </BrowserRouter>
